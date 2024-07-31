3feacda46552aa64a5e541ba4ac589f784a2357d
Jakub Nenczak <kubanenczak@gmail.com>
2024-07-31 15:45:02 +0200

    optimised for5

--- src/ps_sort_small.c
+++ src/ps_sort_small.c
@@ -6,88 +6,65 @@
 /*   By: jnenczak <jnenczak@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 16:45:42 by jnenczak          #+#    #+#             */
-/*   Updated: 2024/07/30 18:54:27 by jnenczak         ###   ########.fr       */
+/*   Updated: 2024/07/31 15:43:56 by jnenczak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
 
-void	ps_sort_small(t_stack *a, t_stack *b)
+void ps_sort_small(t_stack *a, t_stack *b)
 {
-	int	len_a;
-	
-	len_a = ps_get_stack_len(a);
-	if (len_a == 3)
-		ps_sort_three(a);
-	else if (len_a == 4)
-		ps_sort_four(a, b);
-	else
-		ps_sort_five(a, b);
+    int len_a = ps_get_stack_len(a);
+    
+    if (ps_is_sorted(a))
+        return;
+    
+    if (len_a == 2)
+        ps_sx(a, SA);
+    else if (len_a == 3)
+        ps_sort_three(a);
+    else if (len_a == 4)
+        ps_sort_four(a, b);
+    else if (len_a == 5)
+        ps_sort_five(a, b);
 }
 
-void	ps_sort_four(t_stack *a, t_stack *b)
+void ps_sort_four(t_stack *a, t_stack *b)
 {
-	int	min_index;
-
-	if (ps_is_sorted(a))
-		return;
-	min_index = ps_get_min_index(a);
-	if (min_index == 1)
-	{
-		ps_rotate(a);
-		print_operation("RA");
-	}
-	else if (min_index == 2)
-	{
-		while (min_index-- > 0)
-		{
-			ps_rotate(a);
-			print_operation("RA");
-		}
-	}
-	else if (min_index == 3)
-	{
-		ps_reverse_rotate(a);
-		print_operation("RRA");
-	}
-	if (ps_is_sorted(a))
-		return;
-	ps_push_to(a, b);
-	ps_sort_three(a);
-	ps_push_to(b, a);
-	print_operation("PA");
+    int min_index = ps_get_min_index(a);
+    
+    if (min_index == 1)
+        ps_rx(a, b, RA);
+    else if (min_index == 2)
+        ps_rx(a, b, RA), ps_rx(a, b, RA);
+    else if (min_index == 3)
+        ps_rrx(a, b, RRA);
+    
+    if (!ps_is_sorted(a))
+    {
+        ps_px(a, b, PB);
+        ps_sort_three(a);
+        ps_px(a, b, PA);
+    }
 }
 
-void	ps_sort_five(t_stack *a, t_stack *b)
+void ps_sort_five(t_stack *a, t_stack *b)
 {
-	int	min_index;
-
-	min_index = ps_get_min_index(a);
-	if (min_index == 1)
-	{
-		ps_rotate(a);
-		print_operation("RA");
-	}
-	else if (min_index == 2 || min_index == 3)
-	{
-		while (min_index-- > 0)
-		{
-			ps_rotate(a);
-			print_operation("RA");
-		}
-	}
-	else
-	{
-		ps_reverse_rotate(a);
-		print_operation("RRA");
-	}
-	if (ps_is_sorted(a))
-		return ;
-	ps_push_to(a, b);
-	// print_operation("PB");
-	// print_operation("PRINTING AFTER PUSH");
-	// print_stacks(a, b);
-	ps_sort_four(a, b);
-	// print_operation("PRINTING AFTER 4 SORT");
-	// print_stacks(a, b);
+    int min_index = ps_get_min_index(a);
+    
+    if (min_index == 1)
+        ps_rx(a, b, RA);
+    else if (min_index == 2)
+        ps_rx(a, b, RA), ps_rx(a, b, RA);
+    else if (min_index == 3)
+        ps_rrx(a, b, RRA), ps_rrx(a, b, RRA);
+    else if (min_index == 4)
+        ps_rrx(a, b, RRA);
+    
+    if (!ps_is_sorted(a))
+    {
+        ps_px(a, b, PB);
+        ps_sort_four(a, b);
+        ps_px(a, b, PA);
+    }
 }
